99489906a10645bb7f29f17646d61e99a8a41742
shao <ts-zhengrui.shao@rakuten.com>
2023-09-26 16:56:32 +0900

    chap1 sample gets run.

--- src/sandbox.py
+++ src/sandbox.py
@@ -4,9 +4,15 @@ import sys
 import traceback
 
 import mnist_loader
+import network
 
 def main():
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+    net = network.Network([784, 30, 10])
+
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
     return 0
 
 if __name__ == "__main__":
