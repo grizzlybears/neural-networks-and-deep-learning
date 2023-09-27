bbdc7a9ffba59c214519763f038979f361a407e0
shao <ts-zhengrui.shao@rakuten.com>
2023-09-27 17:40:11 +0900

    just some memo

--- src/sandbox.py
+++ src/sandbox.py
@@ -6,12 +6,68 @@ import traceback
 import mnist_loader
 import network
 
+import jsonpickle # pip install jsonpickle
+import json
+import yaml # pip install pyyaml
+
+# Third-party libraries
+import numpy as np
+
+
 def main():
-    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    
+    # training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    # net = network.Network([784, 30, 10])
+    # net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
-    net = network.Network([784, 30, 10])
+    net = network.Network([4, 3, 2])
+    serialized = jsonpickle.encode(net)
+    #print(json.dumps(json.loads(serialized), indent=4))
 
-    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+    #print("===================\n")
+
+    sizes = [4,2,1]
+    print( sizes[1:] )
+    print("===================\n")
+
+    a = np.random.randn(2, 1) 
+    print( a )
+    
+    print("===================\n") 
+    a = np.random.randn(1, 1) 
+    print( a )
+
+    print("======== biases ===========\n") 
+    biases = [np.random.randn(y, 1) for y in sizes[1:]];
+    print( biases ) 
+
+
+    print("======== weights ===========\n")  
+
+    #weights = [np.random.randn(y, x)  for x, y in zip(sizes[:-1], sizes[1:])]
+    #  [4,2] , [2:1]  =>  [(4,2), (2,1)]
+
+
+    #a = zip(sizes[:-1], sizes[1:])
+    #print( a ) 
+
+    #serialized = jsonpickle.encode(a)
+    #print(json.dumps(json.loads(serialized), indent=4))
+
+    weights = [np.random.randn(y, x)  for x, y in zip(sizes[:-1], sizes[1:])]
+    print( weights  ) 
+    
+    print("===================\n") 
+    a = zip(biases, weights) 
+    num = 0
+    for b,w in a:
+        print("  = %d = \n" % num)
+        print(b)
+        print("---\n")
+        print(w)
+
+        num = num + 1 
+
 
     return 0
 
