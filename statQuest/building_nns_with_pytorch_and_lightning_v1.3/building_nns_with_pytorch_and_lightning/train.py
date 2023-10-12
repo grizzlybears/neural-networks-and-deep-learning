301aa4b01b454ced48838ffd881aadd107f7ff58
vampirictutor <vampirictutor@gmail.com>
2023-10-12 12:28:04 +0900

    lightning, in progress

---
+++ statQuest/building_nns_with_pytorch_and_lightning_v1.3/building_nns_with_pytorch_and_lightning/train.py
@@ -0,0 +1,104 @@
+import torch # torch will allow us to create tensors.
+import torch.nn as nn # torch.nn allows us to create a neural network.
+import torch.nn.functional as F # nn.functional give us access to the activation and loss functions.
+from torch.optim import SGD # optim contains many optimizers. Here, we're using SGD, stochastic gradient descent.
+
+import lightning as L  # lightning has tons of cool tools that make neural networks easier
+from torch.utils.data import TensorDataset, DataLoader  # these are needed for the training data
+
+import matplotlib.pyplot as plt # matplotlib allows us to draw graphs.
+import seaborn as sns           # seaborn makes it easier to draw nice-looking graphs.
+
+import pprint
+
+# from pytorch_lightning.utilities.seed import seed_everything # this is added because people on different computers were
+# seed_everything(seed=42)                                     # getting different results.
+
+
+## Create a neural network class by creating a class that inherits from LightningModule
+class BasicLightningTrain(L.LightningModule):
+
+    def __init__(
+            self):  # __init__() is the class constructor function, and we use it to initialize the weights and biases.
+
+        super().__init__()  # initialize an instance of the parent class, L.LightningModule.
+
+        ## Now create the weights and biases that we need for our neural network.
+        ## Each weight or bias is an nn.Parameter, which gives us the option to optimize the parameter by setting
+        ## requires_grad, which is short for "requires gradient", to True. Since we don't need to optimize any of these
+        ## parameters now, we set requires_grad=False.
+        ##
+        ## NOTE: Because our neural network is already fit to the data, we will input specific values
+        ## for each weight and bias. In contrast, if we had not already fit the neural network to the data,
+        ## we might start with a random initalization of the weights and biases.
+        self.w00 = nn.Parameter(torch.tensor(1.7), requires_grad=False)
+        self.b00 = nn.Parameter(torch.tensor(-0.85), requires_grad=False)
+        self.w01 = nn.Parameter(torch.tensor(-40.8), requires_grad=False)
+
+        self.w10 = nn.Parameter(torch.tensor(12.6), requires_grad=False)
+        self.b10 = nn.Parameter(torch.tensor(0.0), requires_grad=False)
+        self.w11 = nn.Parameter(torch.tensor(2.7), requires_grad=False)
+
+        self.final_bias = nn.Parameter(torch.tensor( 0.0 ), requires_grad=True)
+
+        self.learning_rate = 0.01 ## this is for gradient descent. NOTE: we will improve this value later, so, technically
+                                  ## this is just a placeholder until then. In other words, we could put any value here
+                                  ## because later we will replace it with the improved value.
+
+    def forward(self, input):  ## forward() takes an input value and runs it though the neural network
+        ## illustrated at the top of this notebook.
+
+        ## the next three lines implement the top of the neural network (using the top node in the hidden layer).
+        input_to_top_relu = input * self.w00 + self.b00
+        top_relu_output = F.relu(input_to_top_relu)
+        scaled_top_relu_output = top_relu_output * self.w01
+
+        ## the next three lines implement the bottom of the neural network (using the bottom node in the hidden layer).
+        input_to_bottom_relu = input * self.w10 + self.b10
+        bottom_relu_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_output = bottom_relu_output * self.w11
+
+        ## here, we combine both the top and bottom nodes from the hidden layer with the final bias.
+        input_to_final_relu = (scaled_top_relu_output
+                               + scaled_bottom_relu_output
+                               + self.final_bias)
+
+        output = F.relu(input_to_final_relu)
+
+        return output  # output is the predicted effectiveness for a drug dose.
+
+## create the neural network.
+model = BasicLightningTrain()
+
+## print out the name and value for each parameter
+for name, param in model.named_parameters():
+    print(name, param.data)
+
+## Create the different doses we want to run through the neural network.
+## torch.linspace() creates the sequence of numbers between, and including, 0 and 1.
+input_doses = torch.linspace(start=0, end=1, steps=11)
+
+# now print out the doses to make sure they are what we expect...
+print("\ninput_doses:")
+pprint.pprint(input_doses)
+
+## now run the different doses through the neural network.
+output_values = model(input_doses)
+
+## Now draw a graph that shows the effectiveness for each dose.
+##
+## First, set the style for seaborn so that the graph looks cool.
+sns.set(style="whitegrid")
+
+## create the graph (you might not see it at this point, but you will after we save it as a PDF).
+sns.lineplot(x=input_doses,
+             y=output_values.detach(),
+             color='red',
+             linewidth=2)
+
+## now label the y- and x-axes.
+plt.ylabel('Effectiveness')
+plt.xlabel('Dose')
+
+plt.savefig('un_optimized.pdf')
+
